ce4fd877c0411f7216873a4887800c5febaf49d7
Elena-VN <second68@gmail.com>
2019-04-23 11:04:41 +0300

    Редактирование

--- js/setup.js
+++ js/setup.js
@@ -10,24 +10,39 @@ const similarWizardTemplate = document.querySelector('#similar-wizard-template')
 const similarListElement = userDialog.querySelector('.setup-similar-list');
 
 const WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-const WIZARD_SURNAME = ['Топольницкая', 'да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Нионго', 'Ирвинг'];
-const COLORS_COATS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+const WIZARD_SURNAMES = ['Топольницкая', 'да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Нионго', 'Ирвинг'];
+const COLORS_COATS = [
+  'rgb(101, 137, 164)',
+  'rgb(241, 43, 107)',
+  'rgb(146, 100, 161)',
+  'rgb(56, 159, 117)',
+  'rgb(215, 210, 55)',
+  'rgb(0, 0, 0)'
+];
+
 const COLORS_EYES = ['black', 'red', 'blue', 'yellow', 'green'];
 const AMOUNT_WIZARDS = 4;
 
 /**
- * @param {Number} array
+ * @param {Array} array
+ * @returns {Number}
  */
-const getRandomNumbers = function (array) {
+const getRandomIndex = function (array) {
+  if (array.length === 0) {
+    throw new Error('The `array` argument must contain at least one element');
+  }
   return Math.floor(Math.random() * array.length);
 };
 
 // Сборка объекта, заполнение рандомными данными
-const createWizards = function () {
+const createWizard = function () {
+  const wizardName = WIZARD_NAMES[getRandomIndex(WIZARD_NAMES)];
+  const wizardSurname = WIZARD_SURNAMES[getRandomIndex(WIZARD_SURNAMES)];
+
   return {
-    name: WIZARD_NAMES[getRandomNumbers(WIZARD_NAMES)] + ' ' + WIZARD_SURNAME[getRandomNumbers(WIZARD_SURNAME)],
-    coatColor: COLORS_COATS[getRandomNumbers(COLORS_COATS)],
-    eyesColor: COLORS_EYES[getRandomNumbers(COLORS_EYES)]
+    name: wizardName + ' ' + wizardSurname,
+    coatColor: COLORS_COATS[getRandomIndex(COLORS_COATS)],
+    eyesColor: COLORS_EYES[getRandomIndex(COLORS_EYES)]
   }
 };
 
@@ -46,18 +61,11 @@ const renderWizard = function (wizard) {
 };
 
 const wizards = new Array(AMOUNT_WIZARDS);
-for (let i = 0; i < wizards.length; i++) {
-  wizards[i] = createWizards();
-}
-
 const fragment = document.createDocumentFragment();
-
 for (let i = 0; i < wizards.length; i++) {
+  wizards[i] = createWizard();
   fragment.appendChild(renderWizard(wizards[i]));
 }
 
 similarListElement.appendChild(fragment);
-
 document.querySelector('.setup-similar').classList.remove('hidden');
-
-
